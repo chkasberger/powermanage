f35cfecda55163d5a23d4fac281fbd634df697a0
ch.kasberger <ch.kasberger@af96a4e5-e861-c22e-c2fe-fe6be1a76b67>
2012-09-03 21:02:03 +0000

    safe daily work

---
+++ trunk/DataCollector/src/DatacCllector/JUtil.java
@@ -0,0 +1,18 @@
+package DatacCllector;
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class JUtil {
+	public static String getMethodName(int stack) {
+		String functionName;
+		functionName = (new Exception().getStackTrace()[stack].getClassName()
+				+ "." + new Exception().getStackTrace()[stack].getMethodName());
+		return functionName;
+	}
+}
+//http://www.mayor.de/lian98/doc.de/html/g_iec62056_struct.htm
