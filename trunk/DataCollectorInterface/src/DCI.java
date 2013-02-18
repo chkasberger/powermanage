a04ce618f25f670d91f0c0631588e5a209019363
ch.kasberger <ch.kasberger@af96a4e5-e861-c22e-c2fe-fe6be1a76b67>
2013-02-18 21:01:20 +0000

    working copy

--- trunk/DataCollectorInterface/src/DCI.java
+++ trunk/DataCollectorInterface/src/DCI.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.MalformedURLException;
 
-//import org.apache.commons.dbutils.*;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
 
@@ -12,7 +10,7 @@ import DataCollector.JUtil;
 import DataCollector.DB.MySQLAccess;
 import DataCollector.XML.*;
 
-public class Client {
+public class DCI {
 
 	static Logger logger = Logger.getRootLogger();
 	//static Socket socket;
@@ -28,26 +26,16 @@ public class Client {
 	 */
 	public static void main(String[] args){
 		
-		JUtil.setupLogger();
-		logger.setLevel(Level.DEBUG);
+		JUtil.setupLogger(args[1]);
+		
 		logger.debug("app startet");
 		
+		config.getConfig(args[0]);	
 		
-		config(args);
-		
-		//java.util.Date now = new java.util.Date();
-        //SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        //System.out.println(format.format(now));
-
 		MySQLAccess mySQLAccess = new MySQLAccess(config.getMysql());
 		
-		int count = 0;		
 		while(new com.fastcgi.FCGIInterface().FCGIaccept()>= 0) {
-			count ++;
-			logger.debug("ina di loop" + count);
-		   
-			//String input = System.getProperty("FCGI_STDIN");
-			//System.out.println("Content-type: text/html\n\n");
+
 			System.out.println("Content-type: application/json\r\n");
 			System.out.println("<html>");
 			System.out.println("<head><TITLE>FastCGI-Hello Java stdio</TITLE></head>");
@@ -59,19 +47,12 @@ public class Client {
 			JSONObject jObject = null;
 			
 			try {
-				//System.out.println(input);
-				//System.getProperties().list(System.out);
+
 				String queryString = System.getProperty("QUERY_STRING");
 				input = queryString.split("[ _T]");
-				//foo = mySQLAccess.Get(new Object[]{"2013:02:10", "12:00:00", "2013:02:10", "13:00:59", "15"}).toString();
+
 				jObject = mySQLAccess.Get(input);
-				//logger.debug("Array: ");
-		   		//jArray = mySQLAccess.GetJSONArray(input);
-				
-				//System.out.println(jObject);
-		   		//logger.debug("Array: " + jArray);
-		   		//System.out.println("Array: " + jArray);
-				//System.out.flush();
+
 		   		System.out.println(jObject);
 				System.out.flush();
 				logger.debug(input[0] + " " + input[1] + " " + input[2] + " " + input[3]);
@@ -86,11 +67,5 @@ public class Client {
 		   	System.out.println("</body>");
 		   	System.out.println("</html>");		 
 	   }        	    		
-	}
-	
-	private static void config(String[] args) {
-		if(args.length > 0){
-			config.getConfig(args[0]);	
-		}
 	}
 }
